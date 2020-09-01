30d16963ef6e06e9f6d49016e53c89a78f6b2465
Wesley <wesley.contato@hotmail.com>
2020-09-01 20:17:52 -0300

    Lidando com dependências globais

--- projeto-webpack/client/app-src/app.js
+++ projeto-webpack/client/app-src/app.js
@@ -2,7 +2,11 @@ import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'bootstrap/js/modal.js';
 import '../css/meucss.css';
+
+$('h1').click(() => alert('Você me clicou'));
+console.log($('h1').modal);
 
 const controller = new NegociacaoController();
 const negociacao = new Negociacao(new Date(), 1, 200);
