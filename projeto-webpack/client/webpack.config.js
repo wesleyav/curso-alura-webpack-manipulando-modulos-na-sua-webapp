725c6c437a086e3ab9b93a62419e0b4dc0250986
Wesley <wesley.contato@hotmail.com>
2020-09-01 06:45:35 -0300

    Mudando o ambiente com cross-env

--- projeto-webpack/client/webpack.config.js
+++ projeto-webpack/client/webpack.config.js
@@ -1,4 +1,12 @@
 const path = require('path');
+const babiliPlugin = require('babili-webpack-plugin');
+
+let plugins = [];
+
+if (process.env.NODE_ENV = 'production') {
+    plugins.push(new babiliPlugin());
+}
+
 module.exports = {
     entry: './app-src/app.js',
     output: {
@@ -15,5 +23,6 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins
 }
