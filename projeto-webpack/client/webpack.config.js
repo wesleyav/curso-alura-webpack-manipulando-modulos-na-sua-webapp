e8b250b454aa4c8981253898c6fc155d14321f36
Wesley <wesley.contato@hotmail.com>
2020-09-02 08:15:52 -0300

    Otimizando o build com Scope Hoisting

--- projeto-webpack/client/webpack.config.js
+++ projeto-webpack/client/webpack.config.js
@@ -16,6 +16,7 @@ plugins.push(new webpack.ProvidePlugin({
 
 if (process.env.NODE_ENV == 'production') {
     
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
     plugins.push(new babiliPlugin());
 
     plugins.push(new optimizeCSSAssetsPlugin({
