7a3d5852959f625c1dfa576a209f4fd61ecd70fa
Wesley <wesley.contato@hotmail.com>
2020-09-02 19:26:43 -0300

    System.import vs import

--- projeto-webpack/client/app-src/controllers/NegociacaoController.js
+++ projeto-webpack/client/app-src/controllers/NegociacaoController.js
@@ -75,9 +75,9 @@ export class NegociacaoController {
     async importaNegociacoes() {
 
         try {
-            const{ NegociacaoService } = await System.import('../domain/negociacao/NegociacaoService');
+            const{ NegociacaoService } = await import('../domain/negociacao/NegociacaoService');
             const service = new NegociacaoService();
-            
+
             const negociacoes = await service.obtemNegociacoesDoPeriodo();
             console.log(negociacoes);
             negociacoes.filter(novaNegociacao =>
